01bb996a0b4f0e83487e2ad3b0eb20bb7b42b932
kanroot <61951793+kanroot@users.noreply.github.com>
2021-09-07 23:51:08 +0000

    Implementacion ninemanga (#1)
    * metodos reformulados (Capitulos y images), obtencion de capitulos desde la pagina principal del manga, se corregio un bug con respecto a los links con espacios de las images
    
    * obtencion de los links de las imagenes funcionando con espacios y sin espacios
    
    * reformart
    
    * codigo mas limpio
    
    * Quitada redundancia
    
    * Quitada redundancia de codigo
    
    * Implementacion del poblado de imagenes en los capitulos
    
    * Test corregidos
    
    * añadido el test de imagenes
    
    * intento de arquitectura 1
    
    * actualizada dependencias
    
    * agregados tests unitarios
    
    * implementacion de ninemanga
    
    * -añadidos los test de prueba
    
    * Agregado el genero para los mangas
    
    * Create main.yml
    
    * Update main.yml
    
    * Update main.yml
    
    * test
    
    * correciones a los test de genero
    
    * limpieza al main
    
    * limpieza codacy
    
    * Update manga_scrap/modelos.py
    
    Co-authored-by: Gilles <43683714+corp-0@users.noreply.github.com>
    
    * Update manga_scrap/modelos.py
    
    Co-authored-by: Gilles <43683714+corp-0@users.noreply.github.com>
    
    * Update manga_scrap/proveedores/nineMangaNet.py
    
    Co-authored-by: Gilles <43683714+corp-0@users.noreply.github.com>
    
    * Correcion al modelo de los manga_preview, se añadieron los generos.
    
    Co-authored-by: Andrés Riquelme <andresfranco.rs@gmail.com>
    Co-authored-by: Gilles <43683714+corp-0@users.noreply.github.com>

---
+++ manga_scrap/scrapper.py
@@ -0,0 +1,64 @@
+import requests
+from bs4 import BeautifulSoup as BS
+from modelos import Manga, Capitulo
+
+url_lista = "https://ninemanga.net/manga-list"
+
+
+def obtener_mangas(page: int = 1):
+    r = requests.get(f"{url_lista}?page={page}")
+    soup = BS(r.text, features='html.parser')
+    cosa = soup.find_all("div", {"class": "thumbnail"})
+    mangas = []
+    for resultado in cosa:
+        a_parsear = resultado.contents
+        enlace = a_parsear[1].attrs.get("href")
+        imagen = a_parsear[1].contents[1].attrs.get("src")
+        nombre = a_parsear[1].contents[1].attrs.get("alt")
+        capitulos = obtener_capitulos_menos_hard(enlace)
+        manga = Manga(nombre, imagen, enlace, capitulos)
+        mangas.append(manga)
+    return mangas
+
+
+# se obtinen los mangas desde la pagina principal del manga, menos hardcore si son 0 o 1, o incluso alguna wuea 0.0.0.01 xd
+def obtener_capitulos_menos_hard(enlace: str):
+    r = requests.get(enlace)
+    soup = BS(r.text, features='html.parser')
+    table = soup.find('table', attrs={'class': 'table-hover'})
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    capitulos = []
+    for row in rows:
+        enlace_capitulo = (row.contents[1].contents[3].attrs.get("href"))
+        nombre = row.contents[1].contents[3].contents[0]
+        capitulos.append(Capitulo(nombre, enlace_capitulo))
+    return capitulos
+
+
+def obtener_manga_por_pagina_y_index(pagina: int, index: int):
+    manga = obtener_mangas(pagina)[index]
+    obtener_capitulos_menos_hard(manga)
+    obtener_img(manga.capitulos[0])
+
+
+# existen espacios en los links de las images y los deja como 'link base (espacio)' '(espacio) resto de link'
+# por eso recree este metodo
+# falta añadirlos al capitulo, por alguna razon no puedo añadirlo al capitulo
+def obtener_img(capitulo: Capitulo):
+    r = requests.get(capitulo)
+    soup = BS(r.text, features="html.parser")
+    images = soup.find_all("img", {"class": "img-responsive"})
+    lista_images = []
+    for img in images:
+        enlace_bruto = img.attrs.get("data-src")
+        if "None" not in str(enlace_bruto):
+            # evita espacios no manejados
+            lista_images.append(enlace_bruto.strip())
+
+    capitulo.imagenes = lista_images
+
+
+
+if __name__ == "__main__":
+    obtener_manga_por_pagina_y_index(5, 10)
---
+++ tests/test_json_serializable.py
@@ -0,0 +1,70 @@
+import unittest
+import json
+from manga_scrap.modelos import JsonSerializable, Manga, Capitulo, Imagen, Genero
+from dataclasses import dataclass
+
+
+@dataclass()
+class ObjetoSimple(JsonSerializable):
+    campo_str: str
+    campo_int: int
+    campo_bool: bool
+
+    def __eq__(self, other):
+        return (
+                self.campo_int == other.campo_int and
+                self.campo_str == other.campo_str and
+                self.campo_bool == other.campo_bool)
+
+
+class ObjetoSimpleTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.obj = ObjetoSimple("string", 10, True)
+        self.serializado = self.obj.to_json_string()
+        self.deserializado = json.loads(self.serializado)
+
+    def test_campo_str(self):
+        self.assertEqual("string", self.deserializado.get("campo_str"))
+
+    def test_campo_int(self):
+        self.assertEqual(10, self.deserializado.get("campo_int"))
+
+    def test_campo_bool(self):
+        self.assertEqual(True, self.deserializado.get("campo_bool"))
+
+    def test_de_json_a_objeto(self):
+        self.assertEqual(self.obj, ObjetoSimple(**self.deserializado))
+
+
+class MangaSerializacionTest(unittest.TestCase):
+    esperado = {
+        "nombre": "nombre",
+        "imagen": "imagen",
+        "enlace": "enlace",
+        "contenido_adulto": False,
+        "capitulos": [{
+            "nombre": "pepe",
+            "enlace": "enlace_capitulo",
+            "imagenes": [{
+                "enlace": "enlace_imagen"
+            }
+            ]
+        }
+        ],
+        "generos": [{
+            "genero": "Hentai"
+        }]
+    }
+
+    def setUp(self) -> None:
+        img = Imagen("enlace_imagen")
+        capitulo = Capitulo("pepe", "enlace_capitulo", [img])
+        genero = Genero("Hentai")
+        self.manga = Manga("nombre", "imagen", "enlace", [capitulo], [genero])
+
+    def test_manga_serializacion(self):
+        self.assertEqual(self.esperado, json.loads(self.manga.to_json_string()))
+
+
+if __name__ == '__main__':
+    unittest.main()
