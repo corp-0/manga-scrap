e5a6921ba6d2ef3235cb75f7c24cfc91c173ba8e
kan <root.kan.ss@gmail.com>
2021-09-07 20:17:22 +0000

    limpieza al main

--- __main__.py
+++
@@ -1,9 +0,0 @@
-from manga_scrap.proveedores.nineMangaNet import NineMangaNet
-
-if __name__ == "__main__":
-    nine = NineMangaNet()
-    for manga in nine.catalogo:
-        if "Prison School" in manga.nombre:
-            manga_buscado = nine.obtener_manga(manga)
-            nine.obtener_img(manga_buscado.capitulos[0])
-            print(manga_buscado)
